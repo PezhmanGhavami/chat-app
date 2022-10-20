4f94d6eaf2418d2595638308fc06509903cdb1cf
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-10-20 14:57:32 +0000

    Installed and initilezed required packages

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-full h-screen bg-slate-900">
+      <p className="text-2xl text-white">Tailwind added</p>
+    </div>
+  );
+};
+
+export default Home;
