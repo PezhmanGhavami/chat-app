1b1af597df9720a195ae01e2badc01855b0056ca
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-10-22 22:35:33 +0000

    Couldn't get it to work with nextjs api routes - this is just a checkpoint as to save the connection flow in inex page and save the changes in gitignore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useState, ChangeEvent } from "react";
 import type { NextPage } from "next";
+import io from "socket.io-client";
+
+const socket = io("127.0.0.1:3000/api/socket");
 
 const Home: NextPage = () => {
+  const [isConnected, setIsConnected] = useState(
+    socket.connected
+  );
+  const [lastMessage, setLastMessage] = useState(null);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+    socket.on("message", (data) => {
+      setLastMessage(data);
+    });
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("hello!");
+  };
+
   return (
-    <div className="w-full h-screen bg-slate-900">
-      <p className="text-2xl text-white">Tailwind added</p>
+    <div className="App">
+      <div>
+        <p>Connected: {"" + isConnected}</p>
+        <p>Last message: {lastMessage || "-"}</p>
+        <button onClick={sendMessage}>Say hello!</button>
+      </div>
     </div>
   );
 };
