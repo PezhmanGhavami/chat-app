cc93e73302e35eaa662ffed46690c73a434c1286
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-10-30 12:22:12 +0000

    added a loading state to auth layout to prevent falsh of signin/up forms if the user is already signed in

--- client/src/components/auth-layout/auth-layout.component.tsx
+++ client/src/components/auth-layout/auth-layout.component.tsx
@@ -1,6 +1,20 @@
 import { Outlet } from "react-router-dom";
 
+import LoadingSpinner from "../loading-spinner/loading-spinner.component";
+
+import useUser from "../../hooks/useUser";
+
 const AuthLayout = () => {
+  const { user } = useUser();
+
+  if (!user || user.isLoggedIn) {
+    return (
+      <div className="mx-auto mt-96 text-3xl">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="text-center tracking-tight pt-28 text-6xl pb-9">
