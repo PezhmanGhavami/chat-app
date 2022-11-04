5a01d974dd8f49157532701845255a146b7645d3
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-11-04 16:02:30 +0000

    Made the content of the index page unselectable

--- client/src/pages/index/index.page.tsx
+++ client/src/pages/index/index.page.tsx
@@ -1,6 +1,6 @@
 function Index() {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex justify-center items-center h-full select-none">
       <p className="text-xl text-center bg-neutral-800 rounded-xl px-4 py-1">
         Select a chat to start messaging
       </p>
