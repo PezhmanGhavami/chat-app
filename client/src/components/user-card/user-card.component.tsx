6fa25dae5c7c0241f1ac79ab643958c0677a64ae
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-11-06 12:16:45 +0000

    Added a link to chat in the UserCard component

--- client/src/components/user-card/user-card.component.tsx
+++ client/src/components/user-card/user-card.component.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const user: {
   profilePicure: string | null;
   displayName: string;
@@ -12,42 +14,47 @@ const unreadCount = 25;
 
 const UserCard = () => {
   return (
-    <div className="flex py-2 px-3 select-none hover:bg-neutral-700 border-b border-b-neutral-700 last-of-type:border-0">
-      {/* Profile picture */}
-      <div className="flex-none bg-red-500 w-12 h-12 rounded-full overflow-hidden text-3xl flex justify-center items-center">
-        {user?.profilePicure ? (
-          <img
-            src={user.profilePicure}
-            alt={`${user.displayName}'s profile picture`}
-          />
-        ) : (
-          <div>
-            {user.displayName[0].toLocaleUpperCase()}
-          </div>
-        )}
-      </div>
-      {/* Chat details */}
-      <div className="pl-3 w-full">
-        {/* Title and last message time */}
-        <div className="flex justify-between">
-          <h3>Chat title</h3>
-          <span className="text-xs text-neutral-400">
-            00:30
-          </span>
+    <Link
+      className="block border-b border-b-neutral-700 last-of-type:border-0"
+      to={"/chat/test"}
+    >
+      <div className="flex py-2 px-3 select-none hover:bg-neutral-700">
+        {/* Profile picture */}
+        <div className="flex-none bg-red-500 w-12 h-12 rounded-full overflow-hidden text-3xl flex justify-center items-center">
+          {user?.profilePicure ? (
+            <img
+              src={user.profilePicure}
+              alt={`${user.displayName}'s profile picture`}
+            />
+          ) : (
+            <div>
+              {user.displayName[0].toLocaleUpperCase()}
+            </div>
+          )}
         </div>
-        {/* Last message summary and unread messages */}
-        <div className="flex justify-between">
-          <div className="relative w-full mr-2">
-            <p className="absolute inset-0 truncate text-neutral-400">
-              {lastMessage}
-            </p>
+        {/* Chat details */}
+        <div className="pl-3 w-full">
+          {/* Title and last message time */}
+          <div className="flex justify-between">
+            <h3>Chat title</h3>
+            <span className="text-xs text-neutral-400">
+              00:30
+            </span>
           </div>
-          <span className="flex-none bg-neutral-600 rounded-full text-sm text-white tracking-tighter w-5 h-5 flex justify-center items-center">
-            {unreadCount}
-          </span>
+          {/* Last message summary and unread messages */}
+          <div className="flex justify-between">
+            <div className="relative w-full mr-2">
+              <p className="absolute inset-0 truncate text-neutral-400">
+                {lastMessage}
+              </p>
+            </div>
+            <span className="flex-none bg-neutral-600 rounded-full text-sm text-white tracking-tighter w-5 h-5 flex justify-center items-center">
+              {unreadCount}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
