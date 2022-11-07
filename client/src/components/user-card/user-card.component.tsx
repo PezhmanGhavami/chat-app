4d6236c323f1fd00ab6783a1590678c5cfaffa51
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-11-07 17:37:33 +0000

    Removed a conditional in UserCard

--- client/src/components/user-card/user-card.component.tsx
+++ client/src/components/user-card/user-card.component.tsx
@@ -9,7 +9,7 @@ const UserCard = ({ user }: { user: IUser }) => {
     <div className="flex items-center px-3 select-none">
       {/* Profile picture */}
       <div className="flex-none bg-red-500 w-8 h-8 rounded-full overflow-hidden text-2xl flex justify-center items-center">
-        {user?.profilePicure ? (
+        {user.profilePicure ? (
           <img
             src={user.profilePicure}
             alt={`${user.displayName}'s profile picture`}
