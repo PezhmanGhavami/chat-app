2b67bc800b0a2dd83bd97484a0aaafc1415d3973
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-11-08 19:23:54 +0000

    Fixed a bug with the user modal that made the last rendered UserCard data the data for all the modals by moving the modal component inside of UserCard component

--- client/src/components/user-card-container/user-cards-container.component.tsx
+++ client/src/components/user-card-container/user-cards-container.component.tsx
@@ -1,12 +1,3 @@
-import { useState, useContext, MouseEvent } from "react";
-import { VscClose } from "react-icons/vsc";
-import { toast } from "react-toastify";
-
-import Overlay from "../overlay/overlay.component";
-import ProfilePicture from "../profile-picture/profile-picture.component";
-
-import { WebSocketContext } from "../../context/websocket.context";
-
 import UserCard, {
   IUser,
 } from "../user-card/user-card.component";
@@ -15,66 +6,9 @@ interface IUserCardsContainer {
   users: null | IUser[];
 }
 
-const UserModal = ({
-  user,
-  closeModal,
-}: {
-  user: IUser;
-  closeModal: (event: MouseEvent) => void;
-}) => {
-  const socket = useContext(WebSocketContext);
-
-  const handleClick = () => {
-    if (!socket) {
-      return toast.error(
-        "Connection lost\nReconnecting..."
-      );
-    }
-    socket.emit("create-chat", { recipientId: user.id });
-  };
-
-  return (
-    <div className="fixed z-40 inset-x-0 bottom-1/2 w-11/12 md:w-3/5 xl:w-2/5 mx-auto bg-neutral-800 rounded-lg p-4 hover:cursor-default">
-      <div className="flex flex-col">
-        <button
-          type="button"
-          className="p-2 pt-0 self-end"
-          onClick={closeModal}
-        >
-          <VscClose className="h-6 w-6" />
-        </button>
-
-        <div className="flex items-center space-x-4 pb-4">
-          <div className="flex-none w-14 h-14">
-            <ProfilePicture user={user} />
-          </div>
-          <p>{user.displayName}</p>
-        </div>
-
-        <button
-          onClick={handleClick}
-          className="bg-green-700 hover:bg-green-800 w-full rounded-lg p-1"
-        >
-          Click to start chatting
-        </button>
-      </div>
-    </div>
-  );
-};
-
 const UserCardsContainer = ({
   users,
 }: IUserCardsContainer) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const closeModal = (event: MouseEvent) => {
-    event.stopPropagation();
-    setModalIsOpen(false);
-  };
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
   if (!users || users?.length === 0) {
     return (
       <div className="flex justify-center items-center text-center text-xl p-12 sm:p-2 h-full select-none">
@@ -88,18 +22,8 @@ const UserCardsContainer = ({
       {users.map((user) => (
         <div
           key={user.id}
-          onClick={openModal}
           className="hover:bg-neutral-700 hover:cursor-pointer py-3"
         >
-          {modalIsOpen && (
-            <>
-              <Overlay handleClick={closeModal} />
-              <UserModal
-                user={user}
-                closeModal={closeModal}
-              />
-            </>
-          )}
           <UserCard user={user} />
         </div>
       ))}
--- client/src/components/user-card/user-card.component.tsx
+++ client/src/components/user-card/user-card.component.tsx
@@ -1,4 +1,11 @@
+import { useState, useContext, MouseEvent } from "react";
+import { VscClose } from "react-icons/vsc";
+import { toast } from "react-toastify";
+
+import Overlay from "../overlay/overlay.component";
 import ProfilePicture from "../profile-picture/profile-picture.component";
+
+import { WebSocketContext } from "../../context/websocket.context";
 
 export interface IUser {
   id: string;
@@ -10,9 +17,69 @@ interface IUserCard {
   user: IUser;
 }
 
-const UserCard = ({ user }: IUserCard) => {
+const UserModal = ({
+  user,
+  closeModal,
+}: {
+  user: IUser;
+  closeModal: (event: MouseEvent) => void;
+}) => {
+  const socket = useContext(WebSocketContext);
+
+  const handleClick = () => {
+    if (!socket) {
+      return toast.error(
+        "Connection lost\nReconnecting..."
+      );
+    }
+    socket.emit("create-chat", { recipientId: user.id });
+  };
+
   return (
-    <div className="flex items-center px-3 select-none">
+    <div className="fixed z-40 inset-x-0 bottom-1/2 w-11/12 md:w-3/5 xl:w-2/5 mx-auto bg-neutral-800 rounded-lg p-4 hover:cursor-default">
+      <div className="flex flex-col">
+        <button
+          type="button"
+          className="p-2 pt-0 self-end"
+          onClick={closeModal}
+        >
+          <VscClose className="h-6 w-6" />
+        </button>
+
+        <div className="flex items-center space-x-4 pb-4">
+          <div className="flex-none w-14 h-14">
+            <ProfilePicture user={user} />
+          </div>
+          <p>{user.displayName}</p>
+        </div>
+
+        <button
+          onClick={handleClick}
+          className="bg-green-700 hover:bg-green-800 w-full rounded-lg p-1"
+        >
+          Click to start chatting
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const UserCard = ({ user }: IUserCard) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const closeModal = (event: MouseEvent) => {
+    event.stopPropagation();
+    setModalIsOpen(false);
+  };
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  return (
+    <div
+      onClick={openModal}
+      className="flex items-center px-3 select-none"
+    >
       {/* Profile picture */}
       <div className="flex-none w-8 h-8">
         <ProfilePicture user={user} />
@@ -21,6 +88,13 @@ const UserCard = ({ user }: IUserCard) => {
       <div className="pl-2">
         <h3>{user.displayName}</h3>
       </div>
+
+      {modalIsOpen && (
+        <>
+          <Overlay handleClick={closeModal} />
+          <UserModal user={user} closeModal={closeModal} />
+        </>
+      )}
     </div>
   );
 };
