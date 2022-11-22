3410bb623b3c281e14fd9525a1c6ea0173eb1bf0
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-11-22 10:04:57 +0000

    Changed the sizing of the modal

--- client/src/components/modal/modal.component.tsx
+++ client/src/components/modal/modal.component.tsx
@@ -13,7 +13,7 @@ const Modal = ({
   return (
     <>
       <Overlay handleClick={closeModal} />
-      <div className="fixed z-40 inset-x-0 bottom-1/2 translate-y-1/2 w-11/12 md:w-3/5 xl:w-2/5 mx-auto bg-gray-100 dark:bg-neutral-800 rounded-lg p-4 hover:cursor-default shadow-2xl">
+      <div className="fixed z-40 inset-x-0 bottom-1/2 translate-y-1/2 w-11/12 max-w-md mx-auto bg-gray-100 dark:bg-neutral-800 rounded-lg p-4 hover:cursor-default shadow-2xl">
         <button
           type="button"
           title="Close"
