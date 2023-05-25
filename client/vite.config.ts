86b1690cdf3850b968a86e33e0ed1668a681a011
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-05-25 13:07:45 +0000

    Configured VitePWA and renamed some assets

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 //TODO - Don't forget to remove secure false in prod
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Chat App",
+        short_name: "Chat App",
+        description:
+          "A prototype chat application made for educational purposes",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
