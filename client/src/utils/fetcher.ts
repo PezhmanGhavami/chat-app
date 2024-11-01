7b5fc778d60a5672ddc1a8b69ae753cb634bb383
Pezhman Ghavami <pejmanghavami@gmail.com>
2024-11-01 19:51:30 +0000

    test: added logger

--- client/src/utils/fetcher.ts
+++ client/src/utils/fetcher.ts
@@ -1,4 +1,5 @@
 const fetcher = async (input: RequestInfo, init: RequestInit) => {
+  console.log(`${import.meta.env.VITE_SOCKET_URL}${input}`);
   const res = await fetch(`${import.meta.env.VITE_SOCKET_URL}${input}`, init);
 
   if (!res.ok) {
